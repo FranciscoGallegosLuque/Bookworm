9030bd972e548bb3c0438682c4c51e7a690f4076
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-11 20:01:21 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Francisco Manuel Gallegos Luque on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
